c25df0218987b6757eb01b67531d3abd1a405116
Pranav Kural <pranavkural042@gmail.com>
2021-06-30 23:28:54 +0000

    Added Unit 3 - problem set

---
+++ main.py
@@ -0,0 +1,7 @@
+from Unit_3.ps3_hangman import *
+
+
+secretWord = 'apple' 
+lettersGuessed = ['e', 'i', 'k', 'p', 'r', 'a']
+print(isWordGuessed(secretWord, lettersGuessed))
+print(getGuessedWord(secretWord, lettersGuessed))
